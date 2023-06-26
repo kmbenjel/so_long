97383fba84cb21f21c7ac8694a9003c5c9de0f88
Khalid Benjelloun <khalidmailing@gmail.com>
2023-06-26 17:16:41 +0100

    kbenjell

--- so_long.h
+++ so_long.h
@@ -12,10 +12,12 @@
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
-#include "get_next_line/get_next_line.h"
-# include <mlx.h>
 # include <fcntl.h>
+# include <mlx.h>
+
+# define BUFFER_SIZE 32
 
 typedef struct s_data
 {
@@ -28,11 +30,11 @@ typedef struct s_data
 
 typedef struct s_map
 {
-    int player_count;
-    int exit_count;
-    int collectible_count;
-    int width;
-    int height;
-} t_map;
+	int		player_count;
+	int		exit_count;
+	int		collectible_count;
+	int		width;
+	int		height;
+}			t_map;
 
 #endif
