0242ec1802333b4a1a3de15768232c6d2ddd0f68
Khalid Benjelloun <khalidmailing@gmail.com>
2023-06-26 16:31:24 +0100

    kbenjell

--- utils/valid_map.c
+++ utils/valid_map.c
@@ -1,68 +1,66 @@
 #include "../so_long.h"
 
-int is_valid_character(char c)
+int	is_valid_character(char c)
 {
-    return (c == '0' || c == '1' || c == 'C' || c == 'E' || c == 'P');
+	return (c == '0' || c == '1' || c == 'C' || c == 'E' || c == 'P');
 }
 
-int validate_map(const char *filename)
+int	validate_map(const char *filename)
 {
-    int fd;
-    char *line;
-    t_map map = {0};
+	int fd;
+	char *line;
+	t_map map = {0};
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	line = get_next_line(fd);
+	while (line)
+	{
+		int line_len = ft_strlen(line);
+		int i = 0;
 
-    while (get_next_line(fd))
-    {
-        int line_len = ft_strlen(line);
-        int i = 0;
+		if (line_len == 0)
+		{
+			free(line);
+			close(fd);
+			return (0);
+		}
 
-        if (line_len == 0)
-        {
-            free(line);
-            close(fd);
-            return 0;  // Empty line detected
-        }
+		if (map.width == 0)
+			map.width = line_len;
+		else if (line_len != map.width)
+		{
+			free(line);
+			close(fd);
+			return (0);
+			while (line[i])
+			{
+				char c = line[i];
+				if (!is_valid_character(c))
+				{
+					free(line);
+					close(fd);
+					return (0);
+				}
 
-        if (map.width == 0)
-            map.width = line_len;
-        else if (line_len != map.width)
-        {
-            free(line);
-            close(fd);
-            return 0;  // Inconsistent line width
-        }
+				if (c == 'P')
+					map.player_count++;
+				else if (c == 'E')
+					map.exit_count++;
+				else if (c == 'C')
+					map.collectible_count++;
 
-        while (line[i])
-        {
-            char c = line[i];
-            if (!is_valid_character(c))
-            {
-                free(line);
-                close(fd);
-                return 0;  // Invalid character detected
-            }
-
-            if (c == 'P')
-                map.player_count++;
-            else if (c == 'E')
-                map.exit_count++;
-            else if (c == 'C')
-                map.collectible_count++;
-
-            i++;
-        }
-
-        map.height++;
-        free(line);
-    }
-
-    close(fd);
-
-    if (map.player_count != 1 || map.exit_count != 1 || map.collectible_count == 0)
-        return 0;
-    return 1;
+				i++;
+			}
+			map.height++;
+			free(line);
+			line = get_next_line(fd);
+		}
+		close(fd);
+		if (map.player_count != 1 || map.exit_count != 1
+			|| map.collectible_count == 0)
+			return (0);
+		return (1);
+	}
 }
