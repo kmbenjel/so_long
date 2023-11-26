2eaff6ad8f450cc91cca762dee2ed33b18fc3ebb
Khalid Benjelloun <khalidmailing@gmail.com>
2023-11-26 22:45:01 +0100

    edit

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:21 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/29 11:43:21 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/11/26 22:43:43 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
-static char	*ft_strjoin(char *s1, char *s2, int on_off);
-char	*until_nl(char *s);
-char	*after_nl(char *s);
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *str);
+static char	*ft_strjoin_on_off(char *s1, char *s2, int on_off);
+char		*until_nl(char *s);
+char		*after_nl(char *s);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
