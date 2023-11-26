2787b6f7c08ca3334b79cb6b2daefa731f5aa7be
Khalid Benjelloun <khalidmailing@gmail.com>
2023-11-26 04:33:15 +0100

    edit

--- so_long.h
+++ so_long.h
@@ -6,18 +6,17 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 09:20:44 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/06/24 22:54:04 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/11/26 04:33:13 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
 # include <fcntl.h>
 # include <mlx.h>
-
-# define BUFFER_SIZE 32
 
 typedef struct s_data
 {
